d15ba7ce10c1f5c56657af2ff625f558e77f7203
meshankhosla <meshankhosla@gmail.com>
2021-06-20 20:17:37 -0700

    Add some home content

--- src/Content/Home/Home.js
+++ src/Content/Home/Home.js
@@ -1,26 +1,60 @@
 import PortfolioContainer from "../../PortfolioContainer";
 import { Slide } from 'react-awesome-reveal';
 import './Home.css';
-import {Row, Col, Image} from "antd";
+import {Row, Col, Image, Divider} from "antd";
 import Title from "antd/es/typography/Title";
 import Text from "antd/es/typography/Text";
+import profileImg from '../../Assets/profileImg.JPG';
+import {Link} from "react-router-dom";
 
 const Home = () => {
   return (
     <PortfolioContainer>
       <Row className='home-container'>
+
         <Col className='home-left-column'>
           <Slide duration={500} triggerOnce>
-            <Title level={3}>Hi, I'm Meshan</Title>
-            <Text>I'm a student at UC Berkeley studying Computer Science</Text>
+            <Title level={3}>Hi, I'm Meshan Khosla</Title>
+              <Text>
+                I'm a student at UC Berkeley studying Computer Science.
+                I've been programming since 9th grade and I've loved it since. I'm
+                interested in frontend, backend, theory, robotics, or any field where
+                software can be used. Feel free to scroll down to see my skills
+                and things I enjoy doing outside of programming. You can also check out my&nbsp;
+                <Link to='/projects'>projects</Link>, <Link to='/experience'>experiences</Link>,
+                and how to <Link to='/contact'>contact</Link> me!
+              </Text>
+          </Slide>
+        </Col>
+        <Col className='home-right-column'>
+          <Slide duration={500} direction={'right'} triggerOnce>
+            <Image width='75%' className='home-profile-img' alt='Profile Image' src={profileImg} preview={false}/>
           </Slide>
         </Col>
 
-        <Col className='home-right-column'>
-          <Slide duration={500} direction={'right'} triggerOnce>
-            <Image className='home-profile-img' preview={false} src='https://media-exp1.licdn.com/dms/image/C4E03AQHtHy6dt8Bn8Q/profile-displayphoto-shrink_400_400/0/1621569090285?e=1629936000&v=beta&t=s9IlzVf247hNtmefgOXJ6XrGQUywmR2V6aHyWcGS0b4'/>
-          </Slide>
-        </Col>
+        <Divider />
+        <div className='home-skills-title'>
+          <Title>Skills</Title>
+        </div>
+        <Row className='home-skills'>
+          <Col className='home-left-column-skills'>
+            <Slide duration={500} triggerOnce>
+              <Title level={3}>Languages & Libraries</Title>
+              <Text>
+                Skills go here
+              </Text>
+            </Slide>
+          </Col>
+          <Col className='home-right-column-skills'>
+            <Slide duration={500} triggerOnce>
+              <Title level={3}>Concepts & Software</Title>
+              <Text>
+                Skills go here
+              </Text>
+            </Slide>
+          </Col>
+        </Row>
+
       </Row>
     </PortfolioContainer>
   );
