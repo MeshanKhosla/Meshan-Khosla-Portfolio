69dea5ee4fc17291f3e4016a56c21e0bd1a42556
Meshan Khosla <mkhosla@berkeley.edu>
2024-07-19 13:21:35 -0700

    chnge redirect structure

--- src/pages/notes.ts
+++ src/pages/notes.ts
@@ -1,5 +1,17 @@
+// import type { APIRoute } from "astro";
+
+// export const GET: APIRoute = ({ redirect }) => {
+// 	return redirect("https://khosla.risk-regulus.ts.net", 301);
+// }
+
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = ({ redirect }) => {
-	return redirect("https://khosla.risk-regulus.ts.net", 301);
-}
+export const GET: APIRoute = () => {
+    return {
+        status: 301,
+        headers: {
+            Location: "https://khosla.risk-regulus.ts.net",
+        },
+        body: null
+    };
+};
