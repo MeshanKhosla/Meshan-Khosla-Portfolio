422a64fcf11146c378c7e2835501eda8a651fbec
Meshan Khosla <meshankhosla@gmail.com>
2026-09-25 14:49:13 -0700

    Add RSS feed to blog

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
+
+export async function GET(context: APIContext) {
+  const posts = (await getCollection('blog')).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
+
+  return rss({
+    title: 'Meshan Khosla | Blog',
+    description:
+      'Things I\'m learning about distributed systems, software engineering, and anything else. Less of a blog, more of a learning log.',
+    site: context.site ?? 'https://meshan.dev',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`,
+    })),
+  });
+}
